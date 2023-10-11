e5e4875c04b73e1bfe6ddb46c9a084c13220a255
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-10-11 16:52:00 -0300

    criada funcoes para alterar status da zona

--- lib/models/zona.dart
+++ lib/models/zona.dart
@@ -1,4 +1,5 @@
 import 'package:campo_minado_flutter/exceptions/bandeira_em_zona_descoberta_exception.dart';
+import 'package:campo_minado_flutter/exceptions/descobrir_zona_com_bandeira_exception.dart';
 
 /// classe que representa uma zona do campo minado
 /// 0 = coberto
@@ -10,12 +11,29 @@ class Zona {
 
   int get status => _status;
 
-  set status(int status) {
-    if (status == 1 && _status == 2) {
-      throw BandeiraEmZonaDescobertaException(
-          "não se pode colocar bandeira em zona descoberta!");
+  // set status(int status) {
+  //   if (status == 1 && _status == 2) {
+  //     throw BandeiraEmZonaDescobertaException(
+  //         "não se pode colocar bandeira em zona descoberta!");
+  //   }
+  //   _status = status;
+  // }
+
+  void colocarBandeira() {
+    _status = 1;
+  }
+
+  void removerBandeira() {
+    _status = 0;
+  }
+
+  void descobrirZona() {
+    if (_status == 0) {
+      _status = 2;
+    } else {
+      throw DescobrirZonaComBandeiraException(
+          "Não é possivel descobrir zona com bandeira");
     }
-    _status = status;
   }
 
   bool get temBomba => _temBomba;
