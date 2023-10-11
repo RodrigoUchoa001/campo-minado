7acf5d4c66f0b802a9bd680e8962c7e55fda628d
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-10-10 23:47:10 -0300

    criado teste de tabuleiro com todas zonas cobertas no inicio

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:campo_minado_flutter/models/tabuleiro.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -26,5 +27,24 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  group("Testes de Zonas", () {
+    test("testa se todas as zonas começam cobertas", () {
+      int dificuldade = 1;
+      Tabuleiro tabuleiro = Tabuleiro(dificuldade);
+
+      bool temDiferenteDeCoberto = false;
+
+      for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+          if (tabuleiro.tabuleiro[i][j].status != 0) {
+            temDiferenteDeCoberto = true;
+          }
+        }
+      }
+
+      expect(temDiferenteDeCoberto, false);
+    });
   });
 }
