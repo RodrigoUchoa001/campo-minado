4c2864b589a59a624df390e62ab188ec72bf25d7
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-10-10 23:46:52 -0300

    criado construtor que preenche o tabuleiro com zonas cobertas

--- lib/models/tabuleiro.dart
+++ lib/models/tabuleiro.dart
@@ -8,20 +8,37 @@ class Tabuleiro {
   /// 2 = medio = 10x16
   /// 3 = dificil = 24x24
   Tabuleiro(int dificuldade) {
+    // if (dificuldade == 1) {
+    //   criaTabuleiro(8, 8);
+    // } else if (dificuldade == 2) {
+    //   criaTabuleiro(10, 16);
+    // } else if (dificuldade == 3) {
+    //   criaTabuleiro(24, 24);
+    // }
     if (dificuldade == 1) {
-      criaTabuleiro(8, 8);
+      tabuleiro = List.generate(
+        8,
+        (row) => List.generate(
+          8,
+          (col) => Zona(),
+        ),
+      );
     } else if (dificuldade == 2) {
-      criaTabuleiro(10, 16);
+      tabuleiro = List.generate(
+        10,
+        (row) => List.generate(
+          16,
+          (col) => Zona(),
+        ),
+      );
     } else if (dificuldade == 3) {
-      criaTabuleiro(24, 24);
-    }
-  }
-
-  void criaTabuleiro(int x, int y) {
-    for (int i = 0; i < x; i++) {
-      for (int j = 0; j < y; j++) {
-        tabuleiro[x][y] = Zona();
-      }
+      tabuleiro = List.generate(
+        24,
+        (row) => List.generate(
+          24,
+          (col) => Zona(),
+        ),
+      );
     }
   }
 }
