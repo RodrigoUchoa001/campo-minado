376cdb360da72f08744e7b90afbbf2f4b3e2bddd
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-10-17 11:22:55 -0300

    tela de escolha de dificuldade levando as respectivas telas de jogo corretamente

--- lib/ui/screens/tela_de_escolha_de_dificuldade.dart
+++ lib/ui/screens/tela_de_escolha_de_dificuldade.dart
@@ -1,3 +1,4 @@
+import 'package:campo_minado_flutter/ui/screens/tela_de_jogo.dart';
 import 'package:campo_minado_flutter/ui/widgets/botao_padrao.dart';
 import 'package:flutter/material.dart';
 
@@ -16,9 +17,30 @@ class TelaDeEscolhaDeDificuldade extends StatelessWidget {
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 24),
-            BotaoPadrao(titulo: 'Fácil', apertar: () {}),
-            BotaoPadrao(titulo: 'Intermidiário', apertar: () {}),
-            BotaoPadrao(titulo: 'Difícil', apertar: () {}),
+            BotaoPadrao(
+                titulo: 'Fácil',
+                apertar: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (_) => const TelaDeJogo(dificuldade: 1)),
+                  );
+                }),
+            BotaoPadrao(
+                titulo: 'Intermediário',
+                apertar: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (_) => const TelaDeJogo(dificuldade: 2)),
+                  );
+                }),
+            BotaoPadrao(
+                titulo: 'Difícil',
+                apertar: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (_) => const TelaDeJogo(dificuldade: 3)),
+                  );
+                }),
           ],
         ),
       ),
