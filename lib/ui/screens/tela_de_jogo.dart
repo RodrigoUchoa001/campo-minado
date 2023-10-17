9385a4c11b6536e1d190d0206bd1d67febf1d989
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-10-17 11:44:57 -0300

    usando widget para zona

--- lib/ui/screens/tela_de_jogo.dart
+++ lib/ui/screens/tela_de_jogo.dart
@@ -1,5 +1,6 @@
+import 'package:campo_minado_flutter/exceptions/dificuldade_escolhida_invalidada_excepcion.dart';
 import 'package:campo_minado_flutter/models/campo_minado.dart';
-import 'package:campo_minado_flutter/models/zona.dart';
+import 'package:campo_minado_flutter/ui/widgets/zona_widget.dart';
 import 'package:flutter/material.dart';
 
 class TelaDeJogo extends StatefulWidget {
@@ -29,7 +30,8 @@ class _TelaDeJogoState extends State<TelaDeJogo> {
     if (dificuldade == 3) {
       return 'Difícil';
     }
-    return '';
+    throw DificuldadeEscolhidaInvalidaException(
+        'Dificuldade escolhida invalida');
   }
 
   @override
@@ -73,17 +75,7 @@ class _TelaDeJogoState extends State<TelaDeJogo> {
                         }
                       });
                     },
-                    child: Container(
-                      width: 30,
-                      height: 30,
-                      decoration: BoxDecoration(
-                        border: Border.all(),
-                        color: _getZonaColor(zona),
-                      ),
-                      child: Center(
-                        child: _getZonaIcon(zona),
-                      ),
-                    ),
+                    child: ZonaWidget(zona: zona),
                   );
                 },
               ),
@@ -92,32 +84,5 @@ class _TelaDeJogoState extends State<TelaDeJogo> {
         ),
       ),
     );
-  }
-
-  Color _getZonaColor(Zona zona) {
-    if (zona.status == 0) {
-      return Colors.grey; // Zona não descoberta.
-    } else if (zona.status == 1) {
-      return Colors.blue; // Zona com bandeira.
-    } else {
-      return Colors.white; // Zona descoberta.
-    }
-  }
-
-  Widget _getZonaIcon(Zona zona) {
-    if (zona.status == 2) {
-      if (zona.temBomba) {
-        return const Icon(Icons.zoom_out_map, color: Colors.black, size: 24);
-      } else {
-        return Text(
-          zona.bombasAdjacentes > 0 ? zona.bombasAdjacentes.toString() : '',
-          style: const TextStyle(fontSize: 20),
-        );
-      }
-    } else if (zona.status == 1) {
-      return const Icon(Icons.flag, color: Colors.black, size: 24);
-    } else {
-      return Container();
-    }
   }
 }
