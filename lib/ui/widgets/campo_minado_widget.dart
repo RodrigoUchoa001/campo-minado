e04f070f09ffc9f9589670484f0324ed628fab97
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-10-29 22:11:13 -0300

    usando InteractiveViewer para dar zoom no campo minado

--- lib/ui/widgets/campo_minado_widget.dart
+++ lib/ui/widgets/campo_minado_widget.dart
@@ -29,80 +29,91 @@ class _CampoMinadoWidgetState extends State<CampoMinadoWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      shrinkWrap: true,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: widget.campoMinado.tabuleiro[0].length,
+    return InteractiveViewer(
+      // TODO: fazer o interactive viewer pegar a tela toda
+      panEnabled: false, // Set it to false to prevent panning.
+      boundaryMargin: const EdgeInsets.all(double.infinity),
+      minScale: 1.0,
+      maxScale: 3.0,
+      child: GridView.builder(
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: widget.campoMinado.tabuleiro[0].length,
+        ),
+        itemCount: widget.campoMinado.tabuleiro.length *
+            widget.campoMinado.tabuleiro[0].length,
+        itemBuilder: (context, index) {
+          final row = index ~/ widget.campoMinado.tabuleiro[0].length;
+          final col = index % widget.campoMinado.tabuleiro[0].length;
+          final zona = widget.campoMinado.tabuleiro[row][col];
+
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                try {
+                  if (zona.status == 0) {
+                    if (zona.temBomba) {
+                      widget.campoMinado.descobrirZona(row, col);
+                      mostrarMsgDeFimDeJogo(false);
+                    } else {
+                      widget.campoMinado.descobrirZonasAdjacentes(row, col);
+                      if (!widget.campoMinado.jogoEmAndamento) {
+                        mostrarMsgDeFimDeJogo(widget.campoMinado.vitoria);
+                      }
+                    }
+                  }
+                } on DescobrirZonaComBandeiraException catch (e) {
+                  final snackBar = SnackBar(
+                    content: Text(e.toString()),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+
+                if (!widget.campoMinado.jogoEmAndamento) {
+                  for (int i = 0;
+                      i < widget.campoMinado.tabuleiro.length;
+                      i++) {
+                    for (int j = 0;
+                        j < widget.campoMinado.tabuleiro[i].length;
+                        j++) {
+                      if (widget.campoMinado.tabuleiro[i][j].temBomba) {
+                        widget.campoMinado.tabuleiro[i][j]
+                            .forcarDescobrirZona();
+                      }
+                    }
+                  }
+                  mostrarMsgDeFimDeJogo(widget.campoMinado.vitoria);
+                }
+              });
+            },
+            onLongPress: () {
+              setState(() {
+                if (zona.status == 1) {
+                  try {
+                    widget.campoMinado.removerBandeira(row, col);
+                  } on BandeiraEmZonaDescobertaException catch (e) {
+                    final snackBar = SnackBar(
+                      content: Text(e.toString()),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                } else {
+                  try {
+                    widget.campoMinado.colocarBandeira(row, col);
+                  } on BandeiraEmZonaDescobertaException catch (e) {
+                    final snackBar = SnackBar(
+                      content: Text(e.toString()),
+                    );
+                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  }
+                }
+              });
+            },
+            child: ZonaWidget(zona: zona, venceu: widget.campoMinado.vitoria),
+          );
+        },
       ),
-      itemCount: widget.campoMinado.tabuleiro.length *
-          widget.campoMinado.tabuleiro[0].length,
-      itemBuilder: (context, index) {
-        final row = index ~/ widget.campoMinado.tabuleiro[0].length;
-        final col = index % widget.campoMinado.tabuleiro[0].length;
-        final zona = widget.campoMinado.tabuleiro[row][col];
-
-        return GestureDetector(
-          onTap: () {
-            setState(() {
-              try {
-                if (zona.status == 0) {
-                  if (zona.temBomba) {
-                    widget.campoMinado.descobrirZona(row, col);
-                    mostrarMsgDeFimDeJogo(false);
-                  } else {
-                    widget.campoMinado.descobrirZonasAdjacentes(row, col);
-                    if (!widget.campoMinado.jogoEmAndamento) {
-                      mostrarMsgDeFimDeJogo(widget.campoMinado.vitoria);
-                    }
-                  }
-                }
-              } on DescobrirZonaComBandeiraException catch (e) {
-                final snackBar = SnackBar(
-                  content: Text(e.toString()),
-                );
-                ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              }
-
-              if (!widget.campoMinado.jogoEmAndamento) {
-                for (int i = 0; i < widget.campoMinado.tabuleiro.length; i++) {
-                  for (int j = 0;
-                      j < widget.campoMinado.tabuleiro[i].length;
-                      j++) {
-                    if (widget.campoMinado.tabuleiro[i][j].temBomba) {
-                      widget.campoMinado.tabuleiro[i][j].forcarDescobrirZona();
-                    }
-                  }
-                }
-                mostrarMsgDeFimDeJogo(widget.campoMinado.vitoria);
-              }
-            });
-          },
-          onLongPress: () {
-            setState(() {
-              if (zona.status == 1) {
-                try {
-                  widget.campoMinado.removerBandeira(row, col);
-                } on BandeiraEmZonaDescobertaException catch (e) {
-                  final snackBar = SnackBar(
-                    content: Text(e.toString()),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              } else {
-                try {
-                  widget.campoMinado.colocarBandeira(row, col);
-                } on BandeiraEmZonaDescobertaException catch (e) {
-                  final snackBar = SnackBar(
-                    content: Text(e.toString()),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              }
-            });
-          },
-          child: ZonaWidget(zona: zona, venceu: widget.campoMinado.vitoria),
-        );
-      },
     );
   }
 }
